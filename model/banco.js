5f58643792ecf59814349ef4da079812d6cf40f3
Michael Thierry <michael.silva@uniimedaraguaia.coop.br>
2025-02-26 16:56:48 -0300

    feat: criada funcao de acesso ao banco

--- model/banco.js
+++ model/banco.js
@@ -1,2 +1,21 @@
 // IMPORTACOES
+require('dotenv').config({path: '.env'})
 const banco = require('oracledb')
+
+// FUNCAO DE CONEXAO COM BANCO
+const conectarBanco = async () => {
+    // Estabelece conexao no banco
+    try {
+        // conecta no banco
+        const conexao = await banco.getConnection({
+            user: process.env.USER,
+            password: process.env.PASS,
+            connectString: process.env.CONNECT
+        })
+        console.log("Conectado ao Banco")
+        // retonra o conector
+        return conexao
+    }catch(erro) {
+        throw erro; // Lançando erro para tratamento externo
+    }   
+}
