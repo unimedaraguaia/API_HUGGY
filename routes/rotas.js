932d9438dd99bf7eca315d10527c2c7da1737125
Michael Thierry <michael.silva@uniimedaraguaia.coop.br>
2025-03-11 10:40:18 -0300

    feat: adicao de rota de boleto

--- routes/rotas.js
+++ routes/rotas.js
@@ -5,6 +5,7 @@ const controlador = require('../controller/controlador')
 // CONFIGURANDO ROTAS
 rotas.get('/usuario/:cpf', controlador.buscarUsuario)
 rotas.get('/titular/:carteira', controlador.buscarCodigoTitular)
+rotas.get('/boleto/:codigoTitular', controlador.buscarBoleto)
 
 // EXPORTACOES
 module.exports = rotas
