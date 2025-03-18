af11c79c7a907f898aaaca305929116c0edd8e1c
Michael Thierry <michael.silva@uniimedaraguaia.coop.br>
2025-03-18 08:19:14 -0300

    feat: criada novas funçoes

--- model/banco.js
+++ model/banco.js
@@ -1,18 +1,28 @@
 // IMPORTACOES
 const banco = require('oracledb')
 //require('dotenv').config()
+
 /**
  * Funçao que conecta com o banco de dados
  * @returns um conector para o banco em caso de sucesso ou erro a ser tratado
  */
 const conectarBanco = async () => {
+    
+    banco.initOracleClient({ libDir: process.env.PATH_ORACLE })
+    
     // Tenta estabelecer conexao no banco
     try {
         // conecta no banco
         const conexao = await banco.getConnection({
+        
             user: process.env.USER,
             password: process.env.PASS,
             connectString: process.env.CONNECT
+        
+            /*user: "solus",
+            password: "crm",
+            connectString: "192.0.0.15:1521/crmtst"*/
+
         })
         // retorna o conector
         return conexao
@@ -21,8 +31,22 @@ const conectarBanco = async () => {
     }   
 }
 
+const desconectarBanco = async (conector) => {
+    // Se a conexão ainda existe
+    if(conector) {
+        // tenta fechar a conexao
+        try {
+            // fecha a conexao
+            await conector.close()
+        }catch(erro){
+            // lanca excessão caso nao consiga fechar a conexão
+            throw erro;
+        }
+    }
+}
+
 /**
- * Funcção que pega o nome de um usuário do banco pelo cpf
+ * Função que pega o nome de um usuário do banco pelo cpf
  * @param {*} cpf cpf do usuário que se deseja o nome 
  * @returns nome do benficiario em caso de sucesso e um erro caso não seja encontrado
  */
@@ -49,17 +73,8 @@ const pegaNomeUsuario = async (cpf) => {
         throw erro
 
     }finally {
-        // Se a conexão ainda existe
-        if(DB) {
-            // tenta fechar a conexao
-            try {
-                // fecha a conexao
-                await DB.close()
-            }catch(erro){
-                // lanca excessão caso nao consiga fechar a conexão
-                throw erro;
-            }
-        }
+        // desconecta o banco
+        desconectarBanco(DB)
     }
 }
 
@@ -77,7 +92,7 @@ const buscarTitularCarteira = async (carteira) => {
         BD = await conectarBanco()
 
         const consulta = await BD.execute(
-            `select u.nnumetitu
+            `select u.nnumetitu, u.cnomeusua
              from hssusua u
              where u.ccodiusua = :carteira and u.csituusua = 'A'`,
              {carteira},
@@ -90,20 +105,12 @@ const buscarTitularCarteira = async (carteira) => {
         throw erro
 
     }finally {
-         // Se a conexão ainda existe
-         if(BD) {
-            // tenta fechar a conexao
-            try {
-                // fecha a conexao
-                await BD.close()
-            }catch(erro){
-                // lanca excessão caso nao consiga fechar a conexão
-                throw erro;
-            }
-        }
+        // desconecta o banco
+        desconectarBanco(BD)
     }
 } 
 
+// FUNÇÃO OBSOLETA (APAGAR)
 /**
  * Busca o id do boleto de um titular apartir da data corrente
  * @param {*} codigoTitular 
@@ -115,39 +122,103 @@ const buscaIdBoleto = async (codigoTitular) => {
         BD = await conectarBanco()
 
         const consulta = await BD.execute(
-            `select P.nnumerpaga, P.dvencpaga, P.nvencpaga
+            `select P.nnumepaga, P.dvencpaga, P.nvencpaga
              from hsspaga P
              where P.nnumetitu = :codigoTitular and P.Dvencpaga >= '05/10/2024' and P.cpagopaga = 'N'`,
              {codigoTitular},
              {outFormat:banco.OUT_FORMAT_OBJECT}
         )
+        // 
+        //console.log(consulta.rows[0]['DVENCPAGA'])
+        const data = new Date(consulta.rows[0]['DVENCPAGA'])
+        const DataFormatada = data.toLocaleDateString('pt-BR')
+        //console.log(DataFormatada)
+        consulta.rows[0]['DVENCPAGA'] = DataFormatada
 
+        console.log(consulta)
         return consulta
 
     }catch(erro) {
         throw erro
 
     }finally {
-         // Se a conexão ainda existe
-         if(BD) {
-            // tenta fechar a conexao
-            try {
-                // fecha a conexao
-                await BD.close()
-            }catch(erro){
-                // lanca excessão caso nao consiga fechar a conexão
-                throw erro;
-            }
-        }
+        // desconecta o banco
+        desconectarBanco(DB)
     }
 }
 
-const linhaPagamento = async (idPagamento) => {
-    let DB
+
+const buscaIdBoleto2 = async (codigoTitular) => {
+    // variaveis
+    let BD
+    let linhas = []
+
     // tenta conectar ao banco
     try {
+        // conecta para o banco
         BD = await conectarBanco()
 
+        // executa consulta com o banco
+        const boletos = await BD.execute(
+            `select P.nnumepaga, P.dvencpaga, P.nvencpaga
+             from hsspaga P
+             where P.nnumetitu = :codigoTitular and P.Dvencpaga >= '05/08/2024'`,
+             {codigoTitular},
+             {outFormat:banco.OUT_FORMAT_OBJECT}
+        )
+        
+        // formata a data de vencimento dos boletos para o padrão brasileiro
+        formataData(boletos)
+
+        // pega os id dos boletos
+        let idBoletos = pegarIdBoleto(boletos)
+
+        //consulta cada linha digitavel de cada boleto
+        for (let id of idBoletos) {
+
+            // consulta as linhas digitaveis
+            const linhasDigitaveis = await BD.execute(
+                `select B.linha_digitavel
+                from table (pkg_boleto_class.boleto(p_id_pagamento => :id,
+                                        p_id_jpaga     => 0,
+                                        p_id_docu      => 0)) B`,
+                {id},
+                {outFormat:banco.OUT_FORMAT_OBJECT}
+            )
+            // caso haja retonos
+            if (linhasDigitaveis.rows.length > 0) {
+                // adiciona as linhas no vetor de linhas
+                linhas.push(linhasDigitaveis.rows[0])
+            }
+    
+        }
+        // adiciona as linhas digitaveis e modifica os rows do boleto
+        boletos.rows = adicionaLinhasDigitaveis(boletos, linhas)
+        // retorna os boletos
+        console.log(boletos.rows)
+        return boletos
+
+    }catch(erro) {
+        // lanca exeção para chamador
+        throw erro
+
+    }finally {
+        // desconecta o banco
+        desconectarBanco(BD)
+    }
+}
+
+//  FUNÇÃO OBSOLETA (APAGAR)
+const linhaPagamento = async (idPagamento) => {
+    // variável
+    let DB
+
+    // tenta conectar ao banco
+    try {
+        // conecta com o banco
+        BD = await conectarBanco()
+
+        // faz a consulta no banco
         const consulta = await BD.execute(
             `select B.linha_digitavel
              from table (pkg_boleto_class.boleto(p_id_pagamento => :idPagamento,
@@ -157,6 +228,10 @@ const linhaPagamento = async (idPagamento) => {
              {outFormat:banco.OUT_FORMAT_OBJECT}
         )
 
+        // exibe
+        console.log(consulta.rows)
+
+        // retorna
         return consulta
 
     }catch(erro) {
@@ -176,10 +251,67 @@ const linhaPagamento = async (idPagamento) => {
         }
     }
 }
+
+/**
+ * Converte as datas da consulta para o formato PT-BR
+ * @param {} consulta 
+ */
+const formataData = (consulta) => {
+    consulta.rows.forEach(row => {
+        const data = new Date(row['DVENCPAGA'])
+        const DataFormatada = data.toLocaleDateString('pt-BR')
+        row['DVENCPAGA'] = DataFormatada
+    });
+}
+
+/**
+ * Pega os identificadores dos boletos passado 
+ * @param {*} consulta consulta do banco contendo dados dos boletos
+ * @returns vetor contendo os numeros dos identificadores dos boletos
+ */
+function pegarIdBoleto(consulta){
+    vetor = []
+    for(let i = 0; i < consulta.rows.length; i++) {
+        vetor[i] = consulta.rows[i]['NNUMEPAGA']
+    }
+    return vetor
+}
+
+/**
+ * Adiciona as linhas digitaveis de cada boleto
+ * @param {*} boletos consulta do banco com dados dos boletos
+ * @param {*} linhas vetor co linhas digitaveis de cada boleto
+ * @returns // vetor com os dados formatados
+ */
+function adicionaLinhasDigitaveis(boletos, linhas){
+    // variavel
+    let resposta = {}
+    let vetor = []
+    // percorre pelos boletos 
+    for (let i = 0; i < boletos.rows.length; i++) {
+        // adicionando as linhas digitaveis
+        boletos.rows[i]['LINHA_DIGITAVEL'] = linhas[i]['LINHA_DIGITAVEL']
+        // formatando campo de boletos
+        //resposta[`boleto${i + 1}`] = boletos.rows[i]
+        vetor.push(boletos.rows[i])
+    }
+    // adicona resposta ao vetor
+    //vetor.push(resposta)
+
+    // retorna resultados
+    //console.log(vetor)
+    return vetor
+}
+
+//buscaIdBoleto2('62463946')
+
+//linhaPagamento2('6398517')
+
 // EXPORTANDO FUNCOES
 module.exports = {
     pegaNomeUsuario,
     buscarTitularCarteira,
-    buscaIdBoleto, 
+    buscaIdBoleto,
+    buscaIdBoleto2, 
     linhaPagamento
 }
