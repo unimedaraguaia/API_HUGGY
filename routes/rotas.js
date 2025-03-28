bf3b2d3c46be8d4beb98952a750f51a5cdd4c8fc
Michael Thierry <michael.silva@uniimedaraguaia.coop.br>
2025-03-28 14:41:39 -0300

    feat: adicionado novas rotas

--- routes/rotas.js
+++ routes/rotas.js
@@ -6,6 +6,8 @@ const controlador = require('../controller/controlador')
 rotas.get('/usuario/:cpf', controlador.buscarUsuario)
 rotas.get('/titular/:carteira', controlador.buscarCodigoTitular)
 rotas.get('/boleto/:codigoTitular', controlador.buscarBoleto)
+rotas.get('/pagar/:idBoleto', controlador.buscarLinhaEditavel)
+rotas.get('/beneficiario/:digitos', controlador.buscarBeneficiario)
 
 // EXPORTACOES
 module.exports = rotas
