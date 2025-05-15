417ad2f372380027ee05dbef6e8250557416e816
Michael Thierry <michael.silva@uniimedaraguaia.coop.br>
2025-05-15 14:36:04 -0300

    refact: criada rotas de acesso

--- api/routes/rotas.js
+++ api/routes/rotas.js
@@ -4,17 +4,19 @@ const controlador = require('../controller/controlador')
 
 // CONFIGURANDO ROTAS
 // rota para pegar os dados do usuário por meio do cpf
-//rotas.get('/usuario/:cpf', controlador.buscarUsuario)
+rotas.get('/usuario/:cpf', controlador.buscarUsuarioCpf)
 // rota para pegar o codigo do titular por meio de uma carteira
-//rotas.get('/titular/:carteira', controlador.buscarCodigoTitular)
+rotas.get('/titular/:carteira', controlador.buscarCodigoTitular)
 // rota para pegar os boletos por meio do codigo do titular
 rotas.get('/boleto/:codigoTitular', controlador.buscarBoleto)
 // rota para pegar a linha digitavel de um boleto 
 //rotas.get('/pagar/:idBoleto', controlador.buscarLinhaEditavel)
 // rota para pegar dados de beneficiários com indepente se for por cpf ou por carteira
-rotas.get('/beneficiario/:digitos', controlador.buscarBeneficiario)
+
+rotas.get('/beneficiario/:digitos', controlador.buscarBeneficiarioDigitos)
 // rota para acessar o boleto
 rotas.get('/:id', controlador.pegaLink)
+rotas.get('/guias/:numeroGuia', controlador.buscarGuia)
 
 // EXPORTACOES
 module.exports = rotas
