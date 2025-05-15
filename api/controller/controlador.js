9d0b86ff7c41c69f36766c53ff87ff312f6d3b6e
Michael Thierry <michael.silva@uniimedaraguaia.coop.br>
2025-05-15 14:30:56 -0300

    refact: modificado controladores

--- api/controller/controlador.js
+++ api/controller/controlador.js
@@ -1,102 +1,112 @@
 // IMPORTACOES
 const banco = require('../model/banco')             // conector de banco
 const shortLinks = require('../util/encurtador')    // mapeador de links encurtados
+const beneficiario = require('../model/beneficiario')
+const boleto = require("../model/boleto")
+
+const Beneficiario = new beneficiario.Beneficiario()
+const Boleto = new boleto.Boleto()
 
 /**
  * Busca um usuario no banco pelo cpf
  * @param {*} req requisição com o cpf da pessoa do qual se deseja o nome
  * @param {*} res resposta em JSON para status da consulta
  */
-/*const buscarUsuario = async (req, res) => {
+const buscarUsuarioCpf = async (req, res) => {
     const { cpf } = req.params;
     try {
-        const resultado = await banco.pegaNomeUsuario(cpf)
+        const resultado = await Beneficiario.buscarTitularCpf(cpf)
         if (resultado.rows.length > 0) {
-            res.status(200).json({ 
-                mensagem:"200",
-                titular: resultado.rows[0]
-             });
-        } else {
-            res.status(200).json({
-                mensagem: "404",
-                titular:{
-                    "NNUMETITU":""
-                } 
-            });
+            res.status(200).json(
+                { 
+                    mensagem:"200",
+                    titular: resultado.rows[0]
+                }
+            );
+        } 
+        else {
+            res.status(200).json(
+                {
+                    mensagem: "404"
+                }
+            );
         }
-    }catch(erro){
-        res.status(200).json({ 
-            mensagem: "500", 
-            titular:{
-                "NNUMETITU":""
-            } 
-        });
     }
-}*/
+    catch(erro) {
+        res.status(200).json(
+            { 
+                mensagem: "500"
+            }
+        );
+    }
+}
 
 /**
  * Busca um titular por meio do numero de carteirinha
  * @param {*} req numero da cateirinha
  * @param {*} res json com a mensagem e o resultado
  */
-/*const buscarCodigoTitular = async (req, res) => {
+const buscarCodigoTitular = async (req, res) => {
     const { carteira } = req.params;
     try {
-        const resultado = await banco.buscarTitularCarteira(carteira)
+        const resultado = await Beneficiario.pegarNomeUsuario(carteira)
         if (resultado.rows.length > 0) {
-            res.status(200).json({ 
-                mensagem:"200",
-                titular: resultado.rows[0]
-             });
-        } else {
-            res.status(200).json({
-                mensagem: "404",
-                titular:{
-                    "NNUMETITU":""
-                } 
-            });
+            res.status(200).json(
+                { 
+                    mensagem:"200",
+                    titular: resultado.rows[0]
+                }
+            );
+        } 
+        else {
+            res.status(200).json(
+                {
+                    mensagem: "404"
+                }
+            );
             
         }
-    }catch(erro){
-        console.error("CONEX> ERRO AO ACESSAR BANCO:", erro);
-        res.status(200).json({ 
-            mensagem: "500", 
-            titular:{
-                "NNUMETITU":""
-            } 
-        });
     }
-}*/
+    catch(erro) {
+        res.status(200).json(
+            { 
+                mensagem: "500"
+            }
+        );
+    }
+}
 
 /**
  * Busca um beneficiário por meio dos digitos passados.
  * @param {*} req são os digitos que podem ser cpf ou o codigo de carteirinha
  * @param {*} res json com a mensagem e o resultado.
  */
-const buscarBeneficiario = async (req, res) => { 
+const buscarBeneficiarioDigitos = async (req, res) => { 
     const { digitos } = req.params;
     try {
-        const resultado = await banco.buscaBeneficiario(digitos)
+        const resultado = await Beneficiario.buscarBeneficiarioTitularBoleto(digitos)
         if (resultado.rows.length > 0) {
-            res.status(200).json({ 
-                mensagem:"200",
-                titular: resultado.rows[0]
-             });
-        } else {
-            res.status(200).json({
-                mensagem: "404",
-                titular:{
-                    "NNUMETITU":""
+            res.status(200).json(
+                { 
+                    mensagem:"200",
+                    titular: resultado.rows[0]
+                }
+            );
+        } 
+        else {
+            res.status(200).json(
+                {
+                    mensagem: "404",
                 } 
-            });
+            );
         }
-    }catch(erro){
-        res.status(200).json({ 
-            mensagem: "500", 
-            titular:{
-                "NNUMETITU":""
+    }
+    catch(erro) {
+        res.status(200).json(
+            { 
+                mensagem: "500", 
             } 
-        });
+        );
     }
 }
 
@@ -108,29 +118,28 @@ const buscarBeneficiario = async (req, res) => {
 const buscarBoleto = async (req, res) => {
     const { codigoTitular } = req.params;
     try {
-        const resultado = await banco.buscaIdBoleto2(codigoTitular)
+        
+        const resultado = await Boleto.buscarBoletosTitular(codigoTitular)
         if (resultado.rows.length > 0) {
-            resposta = {mensagem:'200'}
+            resposta = { mensagem:'200' }
             for (let i = 0; i < resultado.rows.length; i++) {
                 resposta[`boleto${i + 1}`] = resultado.rows[i]
             }
             res.status(200).json(resposta);
+
         } else {
-            res.status(200).json({
-                mensagem: "404",
-                boletos:{
-                    "NNUMETITU":""
-                } 
-            });
-            
+            res.status(200).json(
+                {
+                    mensagem: "404"
+                }
+            );
         }
     }catch(erro){
-        res.status(200).json({ 
-            mensagem: "500", 
-            boletos:{
-                "NNUMETITU":""
-            } 
-        });
+        res.status(200).json(
+            { 
+                mensagem: "500"
+            }
+        );
     }
 }
 
@@ -183,9 +192,40 @@ const pegaLink = async (req, res) => {
     res.status(404).send('Link não encontrado');
 }
 
+const buscarGuia = async (req, res) => {
+    const { numeroGuia } = req.params
+    try{
+        const resultado = await banco.pegarStatusGuia(numeroGuia)
+        if(resultado.rows.length > 0){
+            res.status(200).json({ 
+                mensagem:"200",
+                guia: resultado.rows[1]
+             });
+        } else{
+            res.status(200).json(
+                {
+                    mensagem:"404",
+                }
+            )
+        }
+    }catch(erro){
+        res.status(200).json(
+            {
+                mensagem:"500",
+            }
+        )
+        console.error(erro)
+
+    }
+
+}
+
 // EXPORTAÇÃO
 module.exports = {
     buscarBoleto,
-    buscarBeneficiario, 
-    pegaLink
+    buscarBeneficiarioDigitos, 
+    pegaLink,
+    buscarGuia, 
+    buscarCodigoTitular, 
+    buscarUsuarioCpf
 }
