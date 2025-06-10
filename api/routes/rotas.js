80ef276797272b87ceeac6b3b11c9c6c180c0e0d
Michael Thierry <michael.silva@uniimedaraguaia.coop.br>
2025-06-10 08:05:34 -0300

    refact: adicionada novas rotas para guias

--- api/routes/rotas.js
+++ api/routes/rotas.js
@@ -3,20 +3,15 @@ const rotas = require('express').Router()
 const controlador = require('../controller/controlador')
 
 // CONFIGURANDO ROTAS
-// rota para pegar os dados do usuário por meio do cpf
-rotas.get('/usuario/:cpf', controlador.buscarUsuarioCpf)
-// rota para pegar o codigo do titular por meio de uma carteira
-rotas.get('/titular/:carteira', controlador.buscarCodigoTitular)
+
+rotas.get('/titular/:cpf', controlador.buscarTitular)
+// rota para pegar id e nome de um titular que tenha acesso a boletos
+rotas.get('/titularBoleto/:digitos', controlador.buscarTitularBoletoDigitos)
 // rota para pegar os boletos por meio do codigo do titular
 rotas.get('/boleto/:codigoTitular', controlador.buscarBoleto)
-// rota para pegar a linha digitavel de um boleto 
-//rotas.get('/pagar/:idBoleto', controlador.buscarLinhaEditavel)
-// rota para pegar dados de beneficiários com indepente se for por cpf ou por carteira
-
-rotas.get('/beneficiario/:digitos', controlador.buscarBeneficiarioDigitos)
-// rota para acessar o boleto
-rotas.get('/:id', controlador.pegaLink)
+// rota de aceso aos dados da guia pelo numero da mesma
 rotas.get('/guias/:numeroGuia', controlador.buscarGuia)
-
+// lista todas as guias de um beneficiario titular
+rotas.get('/listarGuias/:codigoTitular', controlador.listarGuias)
 // EXPORTACOES
-module.exports = rotas
+module.exports = rotas 
