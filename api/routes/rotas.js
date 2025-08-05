30a59aa800c436bcfe9ba5cb7b10b5ee34f99180
Michael Thierry <michael.silva@uniimedaraguaia.coop.br>
2025-08-05 10:48:16 -0300

    doc: adicionado commentarios no codigo e modificada as rotas

--- api/routes/rotas.js
+++ api/routes/rotas.js
@@ -2,12 +2,13 @@
 const rotas = require('express').Router()
 const controlador = require('../controller/controlador')
 
+
 // CONFIGURANDO ROTAS
 rotas.get('/titular/:cpf', controlador.buscarTitular)
 // rota para pegar id e nome de um titular que tenha acesso a boletos
-rotas.get('/titularBoleto/:digitos', controlador.buscarTitularBoletoDigitos)
+rotas.get('/titularBoleto', controlador.buscarTitularBoletoDigitos)
 // rota para pegar os boletos por meio do codigo do titular
-rotas.get('/boleto/:codigoTitular', controlador.buscarBoleto)
+rotas.get('/boleto', controlador.buscarBoleto)
 
 // rota de aceso aos dados da guia pelo numero da mesma
 rotas.get('/guias/:numeroGuia', controlador.buscarGuia)
@@ -15,12 +16,12 @@ rotas.get('/guias/:numeroGuia', controlador.buscarGuia)
 rotas.get('/listarGuias/:codigoTitular', controlador.listarGuias)
 
 // gera proctocolo para atendimentos
-rotas.post('/gerarProtocolo/:idPessoa', controlador.criarProtocolo)
+rotas.post('/gerarProtocolo', controlador.criarProtocolo)
 // adicioan um atendimento no protocolo criado
-rotas.post('/adicionaAtendimento/:idProtocolo', controlador.adicionaAtendimento)
+rotas.post('/adicionaAtendimento', controlador.adicionaAtendimento)
 // rota para adicionar mensagem no atendimento
-rotas.post('/insereMensagemAtendimento/:idAtendimento', controlador.adicionaMensagem)
+rotas.post('/insereMensagemAtendimento', controlador.adicionaMensagem)
 // rota pra fechar atendimento 
-rotas.post('/fecharAtendimento/:idAtendimento', controlador.fecharAtendimento)
+rotas.post('/fecharAtendimento', controlador.fecharAtendimento)
 // EXPORTACOES
 module.exports = rotas 
