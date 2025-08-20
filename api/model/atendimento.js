fc4cec55694877bc50a0d01136668029285ddd3d
Michael Thierry <michael.silva@uniimedaraguaia.coop.br>
2025-08-20 17:25:58 -0300

    refact: codigo de atendimento refatorado

--- api/model/atendimento.js
+++ api/model/atendimento.js
@@ -1,32 +1,15 @@
-//const { stat } = require('fs')
+// ============================  IMPORTAÇÕES ========================== //
 const banco = require('./banco')
 const db = require('oracledb')
 
-/**@class: Atendimento
- * Esta classe contem todos os métodos utilizando para 
- * Criar e manipula atendimentos.
- */
-
+// ========================== CLASSE DE ATENDIMENTO ==================== //
 class Atendimento {
     
-    /**
-     * Este Metodo criar um atendimento e o vincula a um protocolo ja aberto
-     * @param {*} idProtocolo identificador do protocolo ao qual será vinculado
-     * @param {*} idPessoa identificador da pessa que está sendo atendida
-     * @param {*} tipoAtendimento identificador da arvore de atendimento
-     * @returns O retono pode ser um json com os status:
-     * 500 - Erro ao criar atendimento
-     * 200 - Sucesso ao criar atendimento
-     * Exception
-     */
     async criar_atendimento_segunda_via_boleto(idProtocolo, idPessoa, tipoAtendimento) {
-
-        let conexao
-
+        let conexaoBanco
         try {
-            // Estabelece conexão e tenta executar a procedure
-            conexao = await banco.conectarBanco()
-            const resultado = await conexao.execute(
+            conexaoBanco = await banco.conectarBanco()
+            const resultadoOperacao = await conexaoBanco.execute(
                 `
                 BEGIN
                     PKG_ATENDIMENTO_CRM.insere_atendimento(
@@ -49,64 +32,40 @@ class Atendimento {
                 }
             )
             try {
-                // tenta dar um commit  nas alterações de banco
-                conexao.commit()
+                conexaoBanco.commit()
+            } catch(erro) {
+                console.log(`[API] Erro ao criar atendimento para segunda via de boleto (commit)`)
+                conexaoBanco.rollback()
+                throw erro
             }
-            catch(erro) {
-                // capture o erro obtido em caso de falha do commit e tentar dar um desfazer no banco
-                console.log(
-                    `[Atendimento] > cria_atendimento_segunda_via_boleto(Erro ao commitar procedure)`, erro
-                )
-                conexao.rollback()
-            }
-            // Se o erro não for capturado, verifica se o o id do atendimento é maior que zero
-            if(resultado.outBinds.p_id_atendimento > 0) {
-                // Retorna o estatus de sucesso, e o id do atendimento
+            if(resultadoOperacao.outBinds.p_id_atendimento > 0) {
+                console.log(`[API] Sucesso ao criar atendimento para segunda via de boleto`)
                 return {
                     status: "200",
                     atendimento: {
-                        id: resultado.outBinds.p_id_atendimento
+                        id: resultadoOperacao.outBinds.p_id_atendimento
                     }  
                 }
-            }
-            else {
-                // retorna o json  com status de falha
-                console.log(
-                    `[Atendimento] > cria_atendimento_segunda_via_boleto: (Erro id < 0, atendimento nao criado)
-                    ${resultado.outBinds.p_id_atendimento, resultado.outBinds.p_msg_retorno}`
-                )
+            } else {
+                console.log(`[API] Erro ao criar atendimento para segunda via de boleto (id < 0)`)
                 return {
                     status: "500" 
                 }
             }
-        }
-        catch (erro) {
-            //lança exceção
-            console.log(
-                `[Atendimento] > cria_atendimento_segunda_via_boleto: (Excessão lançada)`
-            )
+        } catch (erro) {
+            console.log(`[API] Erro ao criar atendimento para segunda via de boleto (Excessão lançada)`)
             throw erro
         }
         finally {
-            // se conexão ativa, tenta fechar
-            banco.desconectarBanco(conexao)
+            banco.desconectarBanco(conexaoBanco)
         }
     }
 
-    /**
-     * Metodo que adiciona uma mensagem em uma tendimento criado
-     * @param {*} idAtendimento identificador do atendimento criado
-     * @param {*} mensagem mensagem a ser adiconada no atendimento
-     * @returns um json contendo status de sucesso ou falha, ou uma excesão.
-     */
-    async adiciona_mensagem_boletos(idAtendimento, mensagem) {
-
-        let conexao
-
+    async adicionar_mensagem_atendimento_segunda_via_boleto(idAtendimento, mensagem) {
+        let conexaoBanco
         try {
-            // Estabelece comunicação com o banco de dados e tenta excutar a procedure
-            conexao = await banco.conectarBanco()
-            const resultado = await conexao.execute(
+            conexaoBanco = await banco.conectarBanco()
+            const resultadoOperacao = await conexaoBanco.execute(
                 `
                 BEGIN
                     PKG_ATENDIMENTO_CRM.insere_mensagem_atendimento(
@@ -140,69 +99,42 @@ class Atendimento {
                     pMensagemRetorno: { dir: db.BIND_OUT, type: db.STRING, maxSize: 4000 }
                 }
             )
+
             try {
-                // tenta dar um commit nas alterações de banco
-                conexao.commit()
-            }
-            catch (erro) {
-                // Captura o erro obtido em caso de falha do commit e tenta dar um desfazer no banco
-                console.log(
-                    `[Atendimento] > adiciona_mensagem_boletos: (Erro ao commitar procedure)\n${erro}`
-                )
-                conexao.rollback()
+                conexaoBanco.commit()
+            } catch (erro) {
+                console.log(`[API] Erro ao adicionar mensagem no atendimento segunda via de boleto (commit)`)
+                conexaoBanco.rollback()
                 return {
                     status:"500"
                 }
             }
 
-           if(resultado.outBinds.pMensagemRetorno == null || resultado.outBinds.pMensagemRetorno == '') {
-             // se nenhum erro foi capturado retorna mensagem de sucesso
-                console.log(
-                    `[Atendimento] > adiciona_mensagem_boletos: (Sucesso)`
-                )
+           if(resultadoOperacao.outBinds.pMensagemRetorno == null || resultadoOperacao.outBinds.pMensagemRetorno == '') {
+                console.log(`[API] Sucesso ao adicionar mensagem no atendimento segunda via de boleto`)
                 return {
                     status:"200"
                 }
-           }
-           else {
-                console.log(
-                    `[Atendimento] > adiciona_mensagem_boletos: (Erro ao adiciona mensagem)
-                    ${resultado.outBinds.pMensagemRetorno}`
-                )
+           } else {
+                console.log(`[API] Erro ao adicionar mensagem no atendimento segunda via de boleto (bad return)`)
                 return {
                     status:"500"
                 }
             }
-        }
-        catch(erro) {
-            // lança uma excessão
-            console.log(
-                `[Atendimento] > adiciona_mensagem_boletos: (Excessão lançada)\n${erro}`
-            )
+        } catch(erro) {
+            console.log(`[API] Erro ao adicionar mensagem no atendimento segunda via de boleto (excessao)`)
             throw erro
         }
         finally {
-            //se conxão ativa, desabilita
-            banco.desconectarBanco(conexao)
+            banco.desconectarBanco(conexaoBanco)
         } 
     }
 
-    /**
-     * Esse método fecha um atendimento
-     * @param {*} idAtendimento identificador do atendimento
-     * @returns podendo ser um json com status:
-     * 500 - Erro ao fechar atendimento
-     * 200 - Sucesso ao fechar atendimento
-     * Exception - no caso de uma execeção
-     */
-
-    async fechar_atendimento(idAtendimento) {
-        // Tenta executar a  procedure
-        let conexao
+    async fechar_atendimento_crm(idAtendimento) {
+        let conexaoBanco
         try {
-            // tenta fecahr o atendimento
-            conexao = await banco.conectarBanco()
-            const resultado = await conexao.execute(
+            conexaoBanco = await banco.conectarBanco()
+            const resultadoOperacao = await conexaoBanco.execute(
                 `
                 BEGIN
                     -- Call the procedure
@@ -218,71 +150,44 @@ class Atendimento {
                     p_guia: { val: null, type: db.STRING }
                 }
             )
-            // tenta commitar as mudanças no banco
             try {
-                conexao.commit()
-            }
-            catch (erro) {
-                // Captura o erro obtido em caso de falha do commit e tenta dar um desfazer no banco
-                console.log(
-                    `[Atendimento] > fecha_atendimento: (Erro ao commitar procedure)\n${erro}`
-                )
-                conexao.rollback()
+                conexaoBanco.commit()
+            } catch (erro) {
+                console.log(`[API] > fecha_atendimento: (Erro ao commitar procedure)\n${erro}`)
+                conexaoBanco.rollback()
                 return {
                     status:"500"
                 }
             }
-            // tenta verificar se o atendimento foi fechado
+            
             try {
-                const operacao = await this.verificar_atendimento_fechado(idAtendimento, conexao)
-                // se fechado retorno o j
-                if(operacao == true) {
-                    console.error(
-                        `[Atendimento] > fecha_atendimento: (Sucesso)\n`
-                    )
+                const status = await this.verificar_atendimento_fechado(idAtendimento, conexaoBanco)
+                if(status == true) {
+                    console.error(`[API] Sucesso a fechar atendimento`)
                     return {
                         status: "200",
                         mensagem: "Atendimento Fechado"
                     }
-                }else{
-                    console.log(
-                        `[Atendimento] > fecha_atendimento: (Erro: atendimento nao fechado)\n`
-                    )
+                } else {
+                    console.log(`[API] Erro atendimento nao fechado`)
                     return {
                         status: "500",
                         mensagem: "Atendimento ainda aberto"
                     }
                 }
-            }
-            catch(erro) {
-                console.log(
-                    `[Atendimento] > fechar_atendimento (Erro ao verificar fechamento)\n`
-                )
+            } catch(erro) {
+                console.log(`[API] Erro ao verificar se atendimento esta fechado`)
                 throw erro
             } 
-        }
-        catch(erro) {
-            console.log(
-                `[Atendimento] > fechar_atendimento (Erro ao tentar fecahr atendimento)\n`
-            )
+        } catch(erro) {
+            console.log(`[API] Erro ao tentar fechar atendimento`)
             throw erro
         }
     }
 
-    /**
-     * Metodo para verificar se atendimento realmente foi fechado no CRM
-     * @param {*} idAtendimento identificador do atendimento
-     * @param {*} conector conexão com o banco de dados.
-     * @returns O retornos possiveis são
-     * true - em caso de o atendimento esta fechado
-     * false - no caso do atendimento ainda estar aberto ou com status inconclusivo
-     * Exception em cado de erro ao fazer a busca
-     */
-
     async verificar_atendimento_fechado(idAtendimento, conector) {
-        // tenta executar a consulta no banco de dados
-        try{
-            const resultado = await conector.execute(
+        try {
+            const resultadoOperacao = await conector.execute(
                 `
                 select A.cstatatend from crmatend A where A.Nnumeatend =: idAtendimento
                 `,
@@ -290,25 +195,18 @@ class Atendimento {
                     idAtendimento
                 }
             )
-            let status = resultado.rows[0][0]
-            //verifica se o atendimento foi fechado
+            let status = resultadoOperacao.rows[0][0]
             if(status == "F") {
-                console.log(status)
                 return true
-            }
-            else {
-                console.log(status)
+            } else {
                 return false
             }
-        }
-        catch(erro) {
-            // lanca a execption no caso de erro
-            console.log("[Atendimento] verificar_atendimento_fechado (Lançamento de Exceção)")
+        } catch(erro) {
+            console.log(`[API] Erro ao conferir fechamento de atendimento (excessao)`)
             throw erro
         }
     }
 }
-   
-module.exports = {
-    Atendimento
-}
+
+// ========================= EXPORTANDO ================================== //
+module.exports = { Atendimento }
