40a10cd111529dbd43c7422281d15a38386ac20a
Michael Thierry <michael.silva@uniimedaraguaia.coop.br>
2025-08-20 11:04:17 -0300

    refact: codigo refatorado e limpo

--- api/routes/rotas.js
+++ api/routes/rotas.js
@@ -2,26 +2,18 @@
 const rotas = require('express').Router()
 const controlador = require('../controller/controlador')
 
-
-// CONFIGURANDO ROTAS
-rotas.get('/titular/:cpf', controlador.buscarTitular)
-// rota para pegar id e nome de um titular que tenha acesso a boletos
+// ================== ROTAS DE GET ===================== //
+rotas.get('/titular/:cpf',  controlador.buscarTitular)
 rotas.get('/titularBoleto', controlador.buscarTitularBoletoDigitos)
-// rota para pegar os boletos por meio do codigo do titular
 rotas.get('/boleto', controlador.buscarBoleto)
-
-// rota de aceso aos dados da guia pelo numero da mesma
 rotas.get('/guias/:numeroGuia', controlador.buscarGuia)
-// lista todas as guias de um beneficiario titular
 rotas.get('/listarGuias/:codigoTitular', controlador.listarGuias)
 
-// gera proctocolo para atendimentos
+// ======================= ROTAS DE POST ========================= //
 rotas.post('/gerarProtocolo', controlador.criarProtocolo)
-// adicioan um atendimento no protocolo criado
 rotas.post('/adicionaAtendimento', controlador.adicionaAtendimento)
-// rota para adicionar mensagem no atendimento
 rotas.post('/insereMensagemAtendimento', controlador.adicionaMensagem)
-// rota pra fechar atendimento 
 rotas.post('/fecharAtendimento', controlador.fecharAtendimento)
-// EXPORTACOES
+
+// ====================== EXPORTACOES ============================ //
 module.exports = rotas 
