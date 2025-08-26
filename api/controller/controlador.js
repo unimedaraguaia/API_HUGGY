c8563109b86da179e2b9a013a399c73d79f61156
Michael Thierry <michael.silva@uniimedaraguaia.coop.br>
2025-08-26 10:40:26 -0300

    refact: codigo refatorado

--- api/controller/controlador.js
+++ api/controller/controlador.js
@@ -1,85 +1,85 @@
-// IMPORTACOES
-const banco = require('../model/banco')             // conector de banco
-const shortLinks = require('../util/encurtador')    // mapeador de links encurtados
+// ===================== IMPORTACOES ========================== //
+const shortLinks = require('../util/encurtador')        
 const beneficiario = require('../model/beneficiario')
 const boleto = require("../model/boleto")
 const guia = require('../model/guia')
 const protocolo = require('../model/protocolo')
 const atendimento = require('../model/atendimento')
 
-// Classes necessárias
-const Beneficiario = new beneficiario.Beneficiario()
-const Boleto = new boleto.Boleto()
-const Guia = new guia.Guia()
-const Protocolo = new protocolo.Protocolo()
-const Atendimento = new atendimento.Atendimento()
+// ============================== FUNÇÕES =========================================================== //
 
-
-// Por meio dos digitos passado verifica se é um titular com acesso aos boletos
-// Retorna [id, nome] caso encontre.
-
-/**
- * Função que busca o titular que pode obter boletos Online
- * @param {*} req requisição que contem os digitos que podem ser CPF ou numero de carteira
- * @param {*} res resposta para a rota sobre o status da operção executada
- */
-const buscarTitularBoletoDigitos = async (req, res) => { 
-    // Obtendo os dados
-    //const { digitos } = req.params;
+const buscar_titular_boleto_digitos = async (req, res) => { 
     const {digitos} = req.headers
-
     try {
-        // Faz a consulta
-        const resultado = await Beneficiario.buscarTitularBoleto(digitos)
-        // se a contem dados nas linhas
-        if (resultado.rows.length > 0) {
-            // retona o JSON de sucesso
+        const Beneficiario = new beneficiario.Beneficiario()
+        const dadosTitular = await Beneficiario.buscar_titular_boleto(digitos)
+        if (dadosTitular.rows.length > 0) {
+            res.status(200).json(
+                { 
+                    mensagem:"200",
+                    titular: {
+                        numero_titular: dadosTitular.rows[0].NNUMETITU,
+                        nome: dadosTitular.rows[0].CNOMEUSUA,
+                        numero_usua:dadosTitular.rows[0].NNUMEUSUA,
+                        id_pessoa:dadosTitular.rows[0].NNUMEPESS
+                    }
+                }
+            )
+        } else {
+            res.status(200).json(
+                { 
+                    mensagem: "404" 
+                }
+            )
+        }
+    } catch(erro) {
+        res.status(200).json(
+            { 
+                mensagem: "500" 
+            }
+        )
+    }
+}
+
+const buscar_titular_guias = async (req, res) => {
+    const { cpf } = req.params;
+    try {
+        const Beneficiario = new beneficiario.Beneficiario()
+        const dadosTitular = await Beneficiario.buscar_titular_ativo_guias(cpf)
+        if (dadosTitular.rows.length > 0) {
             res.status(200).json(
                 { 
                     mensagem:"200",
                     status:{
                         sucesso:"✅"
-                    }, 
-                    titular: {
-                        numero_titular: resultado.rows[0].NNUMETITU,
-                        nome: resultado.rows[0].CNOMEUSUA,
-                        numero_usua:resultado.rows[0].NNUMEUSUA,
-                        id_pessoa:resultado.rows[0].NNUMEPESS
-                    }
+                    },
+                    titular: dadosTitular.rows[0]
                 }
-            );
-        } // sem dados nas linhas
+            )
+        } 
         else {
-            // retorna JSON de falha
             res.status(200).json(
                 {
-                    mensagem: "404",
-                    status: {
-                        falha: "⚠️"
-                    }
+                    mensagem: "404"
                 } 
-            );
+            )
         }
     }
     catch(erro) {
-        // No caso de erro ou lançamento de exception
         res.status(200).json(
             { 
-                mensagem: "500", 
-                status: {
-                    erro: "🛑"
-                }
+                mensagem: "500"
             } 
-        );
+        )
     }
 }
 
-// Por meio do codigo do titular busca os boletos não pagos do mesmo.
-const buscarBoleto = async (req, res) => {
+const buscar_boletos = async (req, res) => {
     const { numero_titular } = req.headers;
     try {
-        const resultado = await Boleto.buscarBoletosTitular(numero_titular)
-        if (resultado.rows.length > 0) {
+        const Boleto = new boleto.Boleto()
+        const dadosBoletos = await Boleto.buscar_boletos_titular(numero_titular)
+        if (dadosBoletos.rows.length > 0) {
             resposta = {
                 mensagem:'200',
                 status:{
@@ -87,64 +87,33 @@ const buscarBoleto = async (req, res) => {
                    money:"💵",
                    doc:"📄", 
                    link:"🔗"
-                }
+                },
+                arquivos: dadosBoletos.rows.arquivos,
             }
-            for (let i = 0; i < resultado.rows.length; i++) {
-                resposta[`boleto${i + 1}`] = resultado.rows[i]
+            for (let i = 0; i < dadosBoletos.rows.length; i++) {
+                resposta[`boleto${i + 1}`] = dadosBoletos.rows[i]
             }
             res.status(200).json(resposta);
-
         } else {
+            console.log("[API] Boletos nao encontrados")
             res.status(200).json(
                 {
                     mensagem: "404"
                 }
-            );
+            )
         }
-    }
-    catch(erro){
+    } catch(erro) {
+        console.log("[API] Erro ao buscar boletos")
         console.log(erro)
         res.status(200).json(
-            { 
+            {
                 mensagem: "500"
             }
-        );
+        )
     }
 }
 
-const buscarTitular = async (req, res) => {
-    const { cpf } = req.params;
-    try {
-        const resultado = await Beneficiario.ehTitularAtivo(cpf)
-        if (resultado.rows.length > 0) {
-            res.status(200).json(
-                { 
-                    mensagem:"200",
-                    status:{
-                        sucesso:"✅"
-                    },
-                    titular: resultado.rows[0]
-                }
-            );
-        } 
-        else {
-            res.status(200).json(
-                {
-                    mensagem: "404",
-                } 
-            );
-        }
-    }
-    catch(erro) {
-        res.status(200).json(
-            { 
-                mensagem: "500",
-            } 
-        );
-    }
-}
-
-const pegaLink = async (req, res) => {
+const pegar_link = async (req, res) => {
     const destino = shortLinks.get(req.params.id);
     if (destino) {
         console.log('Redirecionando para:', destino);
@@ -153,41 +122,41 @@ const pegaLink = async (req, res) => {
     res.status(404).send('Link não encontrado');
 }
 
-const buscarGuia = async (req, res) => {
+const buscar_guia = async (req, res) => {
     const { numeroGuia } = req.params
-    try{
-        const resultado = await Guia.pegarDadosGuia(numeroGuia)
-        if(resultado.rows.length > 0){
-            
-            res.status(200).json({ 
-                mensagem:"200",
-                guia: resultado.rows[0]
-             });
-        } else{
+    try {
+        const Guia = new guia.Guia()
+        const resultado = await Guia.pegar_dados_guia(numeroGuia)
+        if(resultado.rows.length > 0) {
+            res.status(200).json(
+                { 
+                    mensagem:"200",
+                    guia: resultado.rows[0]
+                }
+            )
+        } else {
             res.status(200).json(
                 {
-                    mensagem:"404",
+                    mensagem:"404"
                 }
             )
         }
-    }catch(erro){
-        console.log(erro)
+    } catch(erro) {
+        console.log("[API] falha ao buscar guia")
         res.status(200).json(
             {
-                mensagem:"500",
+                mensagem:"500"
             }
         )
-        console.error(erro)
-
     }
-
 }
 
-const listarGuias = async (req, res) => {
+const listar_guias = async (req, res) => {
     const { codigoTitular } = req.params
-    try{
-        const resultado = await Guia.listarGuiasBeneficiario(codigoTitular)
-        if(resultado.rows.length > 0){
+    try {
+        const Guia = new guia.Guia()
+        const resultado = await Guia.listar_guias_beneficiario(codigoTitular)
+        if(resultado.rows.length > 0) {
             resposta = { 
                 mensagem:'200',
                 status:{
@@ -198,41 +167,30 @@ const listarGuias = async (req, res) => {
                 resposta[`guia${i + 1}`] = resultado.rows[i]
             }
             res.status(200).json(resposta);
-        } else{
+        } else {
+            console.log('[API] nenhuma guia encontrada')
             res.status(200).json(
                 {
-                    mensagem:"404",
+                    mensagem:"404"
                 }
             )
         }
-    }catch(erro){
-        console.log(erro)
+    } catch(erro) {
+        console.log('[API] Erro ao lista guias')
         res.status(200).json(
             {
-                mensagem:"500",
+                mensagem:"500"
             }
         )
-        console.error(erro)
-
     }
-
 }
 
-/**
- * Função que cria um protocolo pegando apenas o id pessoa do titular
- * @param {*} req identificador pessoa do titular
- * @param {*} res json contendo o status da operação ou uma exception
- */
-const criarProtocolo = async (req, res) => {
-    // Obtem o id pesso da requisição
-    //const {idPessoa} = req.params
-    const {id_pessoa} = req.headers
-
-    // Tenta criar protocolo
+const criar_protocolo_boleto = async (req, res) => {
+    const { id_pessoa } = req.headers
     try {
+        const Protocolo = new protocolo.Protocolo()
         const resultado = await Protocolo.criar_protocolo_segunda_via_boleto(id_pessoa)
         if(resultado.status == '200') {
-            // secesso ao criar protocolo
             res.status(200).json(
                 {
                     mensagem:'200',
@@ -242,20 +200,16 @@ const criarProtocolo = async (req, res) => {
                     }
                 }
             )  
-        } // Se resposta diferente de 200
-        else {
-            // Exibe mensagem e retorna json de falha
-            console.error('[Protocolo] > cria_protocolo_segunda_via_boleto: (Erro Status diferente de 200)\n');
+        } else {
+            console.error('[API] Erro ao criar protocolo status diferente de 200')
             res.status(200).json(
                 {
                     mensagem:'500'
                 }
             )
         }
-    } // em caso de erro
-    catch(erro) {
-        // Exibe mensagem e retorna json de falha
-        console.error('[Protocolo] > cria_protocolo_segunda_via_boleto: (Erro ao criar Protocolo)\n', erro);
+    } catch(erro) {
+        console.error('[API] Erro ao criar protocolo erro do catch')
         res.status(200).json(
             {
                 mensagem:'500'
@@ -264,28 +218,17 @@ const criarProtocolo = async (req, res) => {
     }
 }
 
-/**
- * Função que cria um atendimento 
- * @param {*} req requisição que contem o dados nescessários para criar o atendimento
- * @param {*} res resposta a ser enviada para a requisição
- */
-const adicionaAtendimento = async (req, res) => {
-    // Obtendo os dados do header da requisição
+const adicionar_atendimento = async (req, res) => {
     const { id_protocolo, 
             tipo_atendimento, 
             id_usuario 
-          } = req.headers
-    console.log(id_protocolo, tipo_atendimento, id_usuario)
+    } = req.headers
+
     try {
-        // tenta criar o atendimento
-        const resultado = await Atendimento.criar_atendimento_segunda_via_boleto(id_protocolo, id_usuario, tipo_atendimento)
-        // verifica o resultado obtido, se caso o status for 200 e o id > 0 responde sucesso
+        const Atendimento = new atendimento.Atendimento()
+        const resultado = await Atendimento.criar_atendimento_segunda_via_boleto(id_protocolo, id_usuario, tipo_atendimento)  
         if(resultado.status == "200" && resultado.atendimento.id > 0) {  
-            console.log(
-                `[Controler.adicionaAtendimento] \n
-                > cria_atendimento_segunda_via_boleto \n 
-                (Sucesso atendiento criado)\n`
-            )
+            console.log(`[API] Sucesso ao criar atendimento`)
             res.status(200).json(
                 {
                     mensagem:"200",
@@ -294,26 +237,16 @@ const adicionaAtendimento = async (req, res) => {
                     }
                 }
             )
-        }// caso contrário responde 500 para falha
-        else {
-            console.error(
-                `[Controler.adicionaAtendimento]
-                > cria_atendimento_segunda_via_boleto
-                Erro: não foi possivel criar atendimento status diferente de 200\n`
-            )
+        } else {
+            console.error(`[API] Erro, não foi possivel criar atendimento status diferente de 200`)
             res.status(200).json(
                 {
                     mensagem: "500"
                 }
             )
         }
-    }// em caso de erro 
-    catch(erro) {
-        console.error(
-            `[Controler.adicionaAtendimento]
-            > cria_atendimento_segunda_via_boleto
-            Erro: Lançamento de exception:\n ${erro}`
-        )
+    } catch(erro) {
+        console.error(`[API] Erro a criar atendimento Lançamento de exception`)
         res.status(500).json(
             {   
                 mensagem: "500"
@@ -322,38 +255,21 @@ const adicionaAtendimento = async (req, res) => {
     }
 }
 
-/**
- * Função para adicionar uma mensagem a um atendimento
- * @param {*} req requsiçao conetendo os dados para ser inseridos no atendimento
- * @param {*} res resposta de retorno para a chamada
- */
-const adicionaMensagem = async (req, res) => {
-    // obtendo os dados do body
-    const { id_atendimento } = req.headers
-    const { mensagem } = req.body
-    // Tenta adicionar as informações no atendimento
+const adicionar_mensagem_atendimento_boleto = async (req, res) => {
+    const { id_atendimento, mensagem } = req.headers
     try {
-        const operacao = await Atendimento.adiciona_mensagem_boletos(id_atendimento, mensagem)
-        // caso o obtenha sucesso 
+        const Atendimento = new atendimento.Atendimento()
+        const operacao = await Atendimento.adicionar_mensagem_atendimento_segunda_via_boleto(id_atendimento, mensagem)
         if(operacao.status == "200") {
-            console.log(
-                `[Atendimento] 
-                > adiciona_mensagem_boletos: 
-                (Sucesso ao inserir mensagem)\n`
-            )
+            console.log(`[API] Mensagem adicionada`)
             res.status(200).json(
                 {
                     mensagem: "200",
                     info: `Mensagem adicionada ao atendimento ${id_atendimento}`
                 }
             )
-        }// no caso de falha
-        else {
-            console.error(
-                `[Atendimento] 
-                > adiciona_mensagem_boletos: 
-                (Status diferente de 200)\n`
-            )
+        } else {
+            console.error(`[API] Erro ao adicionar mensagem boletos (Status diferente de 200)`)
             res.status(500).json(
                 {
                     mensagem: "500",
@@ -361,13 +277,8 @@ const adicionaMensagem = async (req, res) => {
                 }
             )
         }
-    }// caso uma exceptions seja lançada
-    catch(erro) {
-        console.error(
-            `[Atendimento] 
-            > adiciona_mensagem_boletos: 
-            (Excessão lançada)\n${erro}`
-        )
+    } catch(erro) {
+        console.error(`[API] Erro ao adicionar mensagem (Excessão lançada)\n${erro}`)
         res.status(500).json(
             {
                 mensagem: "500",
@@ -377,37 +288,21 @@ const adicionaMensagem = async (req, res) => {
     }
 }
 
-/**
- * Função para fechar um determinado atendimento
- * @param {*} req requisição que contem a informação do atendimento a ser fechado
- * @param {*} res resposta de retorno para a chamada
- */
-const fecharAtendimento = async (req, res) => {
-    // Obtendo dados 
-    const {id_atendimento} = req.headers
-    // Tenta fechar o atendimento
+const fechar_atendimento = async (req, res) => {
+    const { id_atendimento } = req.headers
     try {
-        const operacao = await Atendimento.fechar_atendimento(id_atendimento)
-        // em caso de sucesso 
+        const Atendimento = new atendimento.Atendimento()
+        const operacao = await Atendimento.fechar_atendimento_crm(id_atendimento)
         if(operacao.status == "200") {
-            console.log(
-                `[Controler] 
-                > fechar_atendimento: 
-                (Status diferente de 200)\n`
-            )
+            console.log(`[API] Atendimento fechado`)
             res.status(200).json(
                 {
                     mensagem: "200",
                     info: `Atendimento Fechado ${id_atendimento}`
                 }
             )
-        } // em caso de falha
-        else {
-            console.log(
-                `[Atendimento] 
-                > fechar_atendimento: 
-                (Status diferente de 200)\n`
-            )
+        } else {
+            console.log(`[API] Atendimento nao fechado`)
             res.status(500).json(
                 {
                     mensagem: "500",
@@ -415,14 +310,8 @@ const fecharAtendimento = async (req, res) => {
                 }
             )
         }
-    }
-    catch(erro) {
-
-        console.log(
-            `[Atendimento] 
-            > fechar_atendimento: 
-            (Excessão lançada)\n${erro}`
-        )
+    } catch(erro) {
+        console.log(`[API] Atendimento não fechado (excessao)\n${erro}`)
         res.status(500).json(
             {
                 mensagem: "500",
@@ -431,16 +320,16 @@ const fecharAtendimento = async (req, res) => {
         )
     }
 }
-// EXPORTAÇÃO
+// =========================================== EXPORTAÇÃO ============================================== //
 module.exports = {
-    buscarBoleto,
-    buscarTitularBoletoDigitos,
-    buscarTitular,
-    pegaLink,
-    buscarGuia, 
-    listarGuias, 
-    criarProtocolo,
-    adicionaAtendimento,
-    adicionaMensagem,
-    fecharAtendimento
+    buscar_boletos,
+    buscar_titular_boleto_digitos,
+    buscar_titular_guias,
+    pegar_link,
+    buscar_guia, 
+    listar_guias, 
+    criar_protocolo_boleto,
+    adicionar_atendimento,
+    adicionar_mensagem_atendimento_boleto,
+    fechar_atendimento
 }
