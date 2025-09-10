ba745bc329aeb642cb2b19b5535be1fe8f7a2c4f
Michael Thierry <michael.silva@uniimedaraguaia.coop.br>
2025-09-10 08:56:24 -0300

    refact: rotas de guias refatoradas

--- api/routes/rotas.js
+++ api/routes/rotas.js
@@ -6,14 +6,15 @@ const controlador = require('../controller/controlador')
 rotas.get('/titularGuias',  controlador.buscar_titular_guias)
 rotas.get('/titularBoleto', controlador.buscar_titular_boleto_digitos)
 rotas.get('/boleto', controlador.buscar_boletos)
-rotas.get('/guias/:numeroGuia', controlador.buscar_guia)
-rotas.get('/listarGuias/:codigoTitular', controlador.listar_guias)
+rotas.get('/guia', controlador.buscar_guia)
+rotas.get('/listarGuias', controlador.listar_guias)
 
 // ======================= ROTAS DE POST ========================= //
-rotas.post('/gerarProtocolo', controlador.criar_protocolo_boleto)
+rotas.post('/gerarProtocolo', controlador.criar_protocolo)
 rotas.post('/adicionarAtendimentoBoleto', controlador.adicionar_atendimento)
+rotas.post('/adicionarAtendimentoGuias', controlador.adicionar_atendimento)
 rotas.post('/insereMensagemAtendimento', controlador.adicionar_mensagem_atendimento_boleto)
 rotas.post('/fecharAtendimento', controlador.fechar_atendimento)
 
 // ====================== EXPORTACOES ============================ //
-module.exports = rotas 
+module.exports = rotas
